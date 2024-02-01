3bc8b3124fd444d35ded85405626cf611432fdc4
tbohne <tim.bohne@dfki.de>
2024-02-01 14:41:09 +0100

    impl comp set knowl

---
+++ obd_ontology/component_set_knowledge.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author Tim Bohne
+
+from typing import List
+
+
+class ComponentSetKnowledge:
+    """
+    Representation of component-set-related expert knowledge.
+    """
+
+    def __init__(self, component_set: str, includes: List[str], verified_by: List[str]) -> None:
+        """
+        Initializes the component set knowledge.
+
+        :param component_set: component set to be represented
+        :param includes: suspect components assigned to this component set
+        :param verified_by: component set can be verified by checking these suspect components
+        """
+        self.component_set = component_set
+        self.includes = includes
+        self.verified_by = verified_by
+
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the component set knowledge.
+
+        :return: string representation of component set knowledge
+        """
+        return "Component Set: " + self.component_set + "\nIncludes: " + str(self.includes) \
+            + "\nVerified by: " + str(self.verified_by)
