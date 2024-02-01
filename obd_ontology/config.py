e797f7511986ff2a6707717d67b36c4bc9024a11
tbohne <tim.bohne@dfki.de>
2024-02-01 14:55:46 +0100

    impl config + fact rep

---
+++ obd_ontology/config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author Tim Bohne
+
+ONTOLOGY_PREFIX = "http://www.semanticweb.org/nesy_diag_ontology#"
+FUSEKI_URL = "http://127.0.0.1:3030"
+SPARQL_ENDPOINT = "/nesy_diag/sparql"
+DATA_ENDPOINT = "/nesy_diag/data"
+UPDATE_ENDPOINT = "/nesy_diag/update"
