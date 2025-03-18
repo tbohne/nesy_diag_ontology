c47e72dd4b389759c10134e42b7b11cef4567d9f
tbohne <tim.bohne@dfki.de>
2025-03-18 16:42:31 +0100

    clean up KG snapshot

--- nesy_diag_ontology/knowledge_snapshot.py
+++ nesy_diag_ontology/knowledge_snapshot.py
@@ -18,25 +18,32 @@ def knowledge_snapshot_error_code_perspective() -> None:
     print("###########################################################################\n")
     for error_code in qt.query_all_error_code_instances(False):
         print(colored(error_code, "yellow", "on_grey", ["bold"]))
-        print(colored("\t- fault condition:", "blue", "on_grey", ["bold"]),
-              qt.query_fault_condition_by_error_code(error_code, False)[0])
-        print(colored("\t- diag subject occurrences:", "blue", "on_grey", ["bold"]))
-        diag_subject_occurrences = qt.query_diag_subject_by_error_code(error_code, False)
-        for diag_subject_occ in diag_subject_occurrences:
-            print("\t\t-", diag_subject_occ)
+        print(
+            colored("\t- fault condition:", "blue", "on_grey", ["bold"]),
+            qt.query_fault_condition_by_error_code(error_code, False)[0]
+        )
+        print(colored("\t- diag entity occurrences:", "blue", "on_grey", ["bold"]))
+        diag_entity_occurrences = qt.query_diag_entity_by_error_code(error_code, False)
+        for diag_entity_occ in diag_entity_occurrences:
+            print("\t\t-", diag_entity_occ)
 
         print(colored("\t- ordered suspect components:", "blue", "on_grey", ["bold"]))
 
         suspect_components = qt.query_suspect_components_by_error_code(error_code, False)
-        ordered_sus_comp = {int(qt.query_priority_id_by_error_code_and_sus_comp(error_code, comp, False)[0]): comp for
-                            comp
-                            in suspect_components}
+        ordered_sus_comp = {
+            int(qt.query_priority_id_by_error_code_and_sus_comp(error_code, comp, False)[0]): comp
+            for comp in suspect_components
+        }
         for i in range(len(suspect_components)):
             print(colored("\t\t- " + ordered_sus_comp[i], "yellow", "on_grey", ["bold"]))
-            print(colored("\t\t\taffected by:", "blue", "on_grey", ["bold"]),
-                  qt.query_affected_by_relations_by_suspect_component(ordered_sus_comp[i], False))
-            print(colored("\t\t\tverifies:", "blue", "on_grey", ["bold"]),
-                  qt.query_verifies_relation_by_suspect_component(ordered_sus_comp[i], False))
+            print(
+                colored("\t\t\taffected by:", "blue", "on_grey", ["bold"]),
+                qt.query_affected_by_relations_by_suspect_component(ordered_sus_comp[i], False)
+            )
+            print(
+                colored("\t\t\tverifies:", "blue", "on_grey", ["bold"]),
+                qt.query_verifies_relation_by_suspect_component(ordered_sus_comp[i], False)
+            )
         print()
     print("\n----------------------------------------------------------------------\n")
 
@@ -50,10 +57,14 @@ def knowledge_snapshot_component_set_perspective() -> None:
     print("###########################################################################\n")
     for comp_set in qt.query_all_component_set_instances(False):
         print(colored(comp_set, "yellow", "on_grey", ["bold"]))
-        print(colored("\t- verified by:", "blue", "on_grey", ["bold"]),
-              qt.query_verifies_relations_by_component_set(comp_set, False))
-        print(colored("\t- includes:", "blue", "on_grey", ["bold"]),
-              qt.query_includes_relation_by_component_set(comp_set, False))
+        print(
+            colored("\t- verified by:", "blue", "on_grey", ["bold"]),
+            qt.query_verifies_relations_by_component_set(comp_set, False)
+        )
+        print(
+            colored("\t- includes:", "blue", "on_grey", ["bold"]),
+            qt.query_includes_relation_by_component_set(comp_set, False)
+        )
     print("\n----------------------------------------------------------------------\n")
 
 
@@ -66,25 +77,29 @@ def knowledge_snapshot_component_perspective() -> None:
     print("###########################################################################\n")
     for comp in qt.query_all_component_instances(False):
         print(colored(comp, "yellow", "on_grey", ["bold"]))
-        print(colored("\t- affected by:", "blue", "on_grey", ["bold"]),
-              qt.query_affected_by_relations_by_suspect_component(comp, False))
-        print(colored("\t- verifies:", "blue", "on_grey", ["bold"]),
-              qt.query_verifies_relation_by_suspect_component(comp, False))
+        print(
+            colored("\t- affected by:", "blue", "on_grey", ["bold"]),
+            qt.query_affected_by_relations_by_suspect_component(comp, False)
+        )
+        print(
+            colored("\t- verifies:", "blue", "on_grey", ["bold"]),
+            qt.query_verifies_relation_by_suspect_component(comp, False)
+        )
     print("\n----------------------------------------------------------------------\n")
 
 
 def knowledge_snapshot_signal_perspective() -> None:
     """
-    Presents a snapshot of the knowledge currently stored in the KG regarding time series signals.
+    Presents a snapshot of the knowledge currently stored in the KG regarding sensor signals.
     """
     print("###########################################################################")
-    print("KNOWLEDGE SNAPSHOT - TIME SERIES SIGNAL PERSPECTIVE")
+    print("KNOWLEDGE SNAPSHOT - SENSOR SIGNAL PERSPECTIVE")
     print("###########################################################################\n")
-    for signal in qt.query_all_recorded_time_series(False):
+    for signal in qt.query_all_recorded_sensor_signals(False):
         signal_id = signal.split("#")[1]
         print(colored("signal: " + signal.split("#")[1], "yellow", "on_grey", ["bold"]))
-        time_series = qt.query_time_series_by_ts_instance(signal_id, False)[0]
-        print(colored("\t- time series excerpt:", "blue", "on_grey", ["bold"]), time_series[:50], "...")
+        signal_str = qt.query_signal_by_sensor_signal_instance(signal_id, False)[0]
+        print(colored("\t- sensor signal excerpt:", "blue", "on_grey", ["bold"]), signal_str[:50], "...")
     print("\n----------------------------------------------------------------------\n")
 
 
@@ -98,22 +113,32 @@ def knowledge_snapshot_signal_classification_perspective() -> None:
     for signal_classification in qt.query_all_signal_classifications(False):
         signal_classification_id = signal_classification.split("#")[1]
         print(colored(signal_classification_id, "yellow", "on_grey", ["bold"]))
-        print(colored("\t- model id:", "blue", "on_grey", ["bold"]),
-              qt.query_model_id_by_signal_classification_id(signal_classification_id, False)[0])
-        print(colored("\t- uncertainty:", "blue", "on_grey", ["bold"]),
-              qt.query_uncertainty_by_signal_classification_id(signal_classification_id, False)[0])
-        time_series_instance = qt.query_time_series_by_classification_instance(signal_classification_id, False)
-        print(colored("\t- classifies:", "blue", "on_grey", ["bold"]),
-              time_series_instance[0].split("#")[1] if len(time_series_instance) > 0 else "")
+        print(
+            colored("\t- model id:", "blue", "on_grey", ["bold"]),
+            qt.query_model_id_by_signal_classification_id(signal_classification_id, False)[0]
+        )
+        print(
+            colored("\t- uncertainty:", "blue", "on_grey", ["bold"]),
+            qt.query_uncertainty_by_signal_classification_id(signal_classification_id, False)[0]
+        )
+        sensor_signal_instance = qt.query_sensor_signal_by_classification_instance(signal_classification_id, False)
+        print(
+            colored("\t- classifies:", "blue", "on_grey", ["bold"]),
+            sensor_signal_instance[0].split("#")[1] if len(sensor_signal_instance) > 0 else ""
+        )
 
         heatmap_instance = qt.query_heatmap_by_classification_instance(signal_classification_id, False)
         heatmap_id = heatmap_instance[0].split("#")[1] if len(heatmap_instance) > 0 else ""
         if len(heatmap_id) > 0:
             print(colored("\t- produces:", "blue", "on_grey", ["bold"]), heatmap_id)
-            print(colored("\t\t- generation_method:", "blue", "on_grey", ["bold"]),
-                  qt.query_generation_method_by_heatmap(heatmap_id, False)[0])
-            print(colored("\t\t- generated heatmap:", "blue", "on_grey", ["bold"]),
-                  qt.query_heatmap_string_by_heatmap(heatmap_id, False)[0])
+            print(
+                colored("\t\t- generation_method:", "blue", "on_grey", ["bold"]),
+                qt.query_generation_method_by_heatmap(heatmap_id, False)[0]
+            )
+            print(
+                colored("\t\t- generated heatmap:", "blue", "on_grey", ["bold"]),
+                qt.query_heatmap_string_by_heatmap(heatmap_id, False)[0]
+            )
 
         suspect_comp_instance = qt.query_suspect_component_by_classification(signal_classification_id, False)
         suspect_comp_id = suspect_comp_instance[0].split("#")[1] if len(suspect_comp_instance) > 0 else ""
@@ -175,9 +200,9 @@ def knowledge_snapshot_diag_log_perspective() -> None:
         fault_path_id = fault_path_instance[0].split("#")[1] if len(fault_path_instance) > 0 else ""
         print(colored("\t- entails fault path:", "blue", "on_grey", ["bold"]), fault_path_id)
 
-        diag_subject_instance = qt.query_diag_subject_by_diag_log(diag_log_id, False)
-        diag_subject_id = diag_subject_instance[0].split("#")[1]
-        print(colored("\t- created for diag subject:", "blue", "on_grey", ["bold"]), diag_subject_id)
+        diag_entity_instance = qt.query_diag_entity_by_diag_log(diag_log_id, False)
+        diag_entity_id = diag_entity_instance[0].split("#")[1]
+        print(colored("\t- created for diag entity:", "blue", "on_grey", ["bold"]), diag_entity_id)
 
         print(colored("\t- diagnostic steps:", "blue", "on_grey", ["bold"]))
         diag_steps = qt.query_diag_steps_by_diag_log(diag_log_id, False)
@@ -206,19 +231,19 @@ def knowledge_snapshot_fault_path_perspective() -> None:
     print("\n----------------------------------------------------------------------\n")
 
 
-def knowledge_snapshot_diag_subject_perspective() -> None:
+def knowledge_snapshot_diag_entity_perspective() -> None:
     """
-    Presents a snapshot of the knowledge currently stored in the KG regarding diag subjects.
+    Presents a snapshot of the knowledge currently stored in the KG regarding diag entities.
     """
     print("###########################################################################")
-    print("KNOWLEDGE SNAPSHOT - DIAG SUBJECT PERSPECTIVE")
+    print("KNOWLEDGE SNAPSHOT - DIAG ENTITY PERSPECTIVE")
     print("###########################################################################\n")
-    for diag_subject_instance_id, subject_id in qt.query_all_diag_subject_instances(False):
-        diag_subject_instance_id = diag_subject_instance_id.split("#")[1]
-        print(colored(diag_subject_instance_id, "yellow", "on_grey", ["bold"]))
-        print(colored("\t- subject ID: " + subject_id, "blue", "on_grey", ["bold"]))
-        print(colored("\t- error codes recorded in this diag subject:", "blue", "on_grey", ["bold"]))
-        error_codes = qt.query_error_codes_recorded_in_diag_subject(diag_subject_instance_id, False)
+    for diag_entity_instance_id, entity_id in qt.query_all_diag_entity_instances(False):
+        diag_entity_instance_id = diag_entity_instance_id.split("#")[1]
+        print(colored(diag_entity_instance_id, "yellow", "on_grey", ["bold"]))
+        print(colored("\t- entity ID: " + entity_id, "blue", "on_grey", ["bold"]))
+        print(colored("\t- error codes recorded in this diag entity:", "blue", "on_grey", ["bold"]))
+        error_codes = qt.query_error_codes_recorded_in_diag_entity(diag_entity_instance_id, False)
         for code in error_codes:
             print("\t\t-", code)
     print("\n----------------------------------------------------------------------\n")
@@ -226,8 +251,9 @@ def knowledge_snapshot_diag_subject_perspective() -> None:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Knowledge snapshot - shows current content of KG')
-    parser.add_argument('--perspective', type=str, help='perspective of snapshot [expert | diag]',
-                        required=False, default='expert')
+    parser.add_argument(
+        '--perspective', type=str, help='perspective of snapshot [expert | diag]', required=False, default='expert'
+    )
     args = parser.parse_args()
     qt = KnowledgeGraphQueryTool()
 
@@ -251,4 +277,4 @@ if __name__ == '__main__':
         knowledge_snapshot_manual_inspection_perspective()
         knowledge_snapshot_diag_log_perspective()
         knowledge_snapshot_fault_path_perspective()
-        knowledge_snapshot_diag_subject_perspective()
+        knowledge_snapshot_diag_entity_perspective()
