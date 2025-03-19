7766f27473a87d7e26da920f424007fba3e576f1
tbohne <tim.bohne@dfki.de>
2025-03-19 11:34:16 +0100

    impl subcomponent knowledge

---
+++ nesy_diag_ontology/sub_component_knowledge.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author Tim Bohne
+
+
+class SubComponentKnowledge:
+    """
+    Representation of subcomponent-related expert knowledge.
+    """
+
+    def __init__(
+            self, sub_component: str, associated_suspect_component: str, associated_chan: str = "",
+            chan_of_interest: str = ""
+    ) -> None:
+        """
+        Initializes the subcomponent knowledge.
+
+        Although subcomponents can have multiple COI and associated channels based on the ontology (inherited),
+        we restrict it to exactly one channel (hasCOI == hasChannel) for the moment.
+
+        :param sub_component: subcomponent name
+        :param associated_suspect_component: component the subcomponent is element of
+        :param associated_chan: channel associated with the component, i.e., 'hasChannel' relation
+        :param chan_of_interest: proposed channel of interest, i.e., 'hasCOI' relation
+        """
+        self.sub_component = sub_component
+        self.associated_suspect_component = associated_suspect_component
+        self.associated_chan = associated_chan
+        self.chan_of_interest = chan_of_interest
+
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the subcomponent knowledge.
+
+        :return: string representation of subcomponent knowledge
+        """
+        return (
+                "SubComponent: " + self.sub_component
+                + "\nAssociated Suspect Component: " + self.associated_suspect_component
+                + "\nAssociated Channel: " + self.associated_chan
+                + "\nCOI: " + self.chan_of_interest
+        )
